d915e633eddfd05ab2c945982114c8bc369e2e55
ComputerArtt <neues_leben41@protonmail.com>
2019-06-30 11:04:43 +0200

    AWT-Button

---
+++ AWT & Events/AWT-Button/UI.java
@@ -0,0 +1,26 @@
+
+package awt.events;
+
+import java.awt.*;
+
+/**
+ *
+ * @author ComputerArtt
+ */
+public class UI extends Frame{
+    
+    public UI()
+    {
+        Button b = new Button("click");
+        b.setBounds(30, 50, 50, 30);//position of button
+        add(b);//add button into frame
+        setSize(150,150);//frame size
+        setLayout(null);
+        setVisible(true);//frame will be visible, by default not visible
+    }
+
+    public static void main(String[] args) {
+        new UI();
+    }
+    
+}
